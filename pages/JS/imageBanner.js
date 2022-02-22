e26d6cd641deee363461536204c9b921617d5773
이용우 <leeyongwoo@iyong-uui-MacBookPro.local>
2022-02-23 00:00:40 +0900

    Add : 슬라이드 반응형 구현

--- pages/JS/imageBanner.js
+++ pages/JS/imageBanner.js
@@ -1,7 +1,5 @@
+// transform: translateX(0%);
 const bannerContainer = document.querySelector('.bannerContainerMobile');
-const bannerFirst = document.querySelector('.bannerFirst');
-const bannerSecond = document.querySelector('.bannerSecond');
-
 const selectCircle = document.querySelector('.imageSelector');
 const selectorYellow = document.querySelector('.imageSelectorYellow');
 
@@ -19,9 +17,23 @@ const isSelectorYCircleFalse = () => {
   selectCircle.style.backgroundColor = '#e4e4e4';
 };
 
+const isResizeValueDesktop = () => {
+  if (window.innerWidth >= 768) {
+    bannerContainer.style.transform = 'none';
+    selectorYellow.style.backgroundColor = '#f5ca5f';
+    selectCircle.style.backgroundColor = '#e4e4e4';
+  }
+};
+
+const isResizeValueMobile = () => {
+  if (window.innerWidth <= 768) {
+    if ((bannerContainer.style.transform = 'none')) {
+      bannerContainer.style.transform = 'translateX(0%)';
+    }
+  }
+};
+
 selectCircle.addEventListener('click', isSelectCircle);
 selectorYellow.addEventListener('click', isSelectorYCircleFalse);
-
-// if (window.innerWidth >= 768) {
-//   console.log('good');
-// }
+window.addEventListener('resize', isResizeValueDesktop);
+window.addEventListener('resize', isResizeValueMobile);
